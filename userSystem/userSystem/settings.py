7c658c911558fc0e49213ffd5dd95ccdd8a1ba33
it21918 <it21918@hua.gr>
2022-08-14 09:34:53 +0300

    add k&s

--- userSystem/userSystem/settings.py
+++ userSystem/userSystem/settings.py
@@ -82,11 +82,9 @@ WSGI_APPLICATION = 'userSystem.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+   'default': env.db()
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
