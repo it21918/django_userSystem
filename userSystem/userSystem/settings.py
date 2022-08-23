7df4f96a2d2ce99025b943810cd310bf1646e564
it21918 <79996442+it21918@users.noreply.github.com>
2022-08-23 11:19:17 +0300

    Update settings.py

--- userSystem/userSystem/settings.py
+++ userSystem/userSystem/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
