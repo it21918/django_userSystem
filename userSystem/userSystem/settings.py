557fa63f1f3d359793b73f6db094dce70d110f90
it21918 <79996442+it21918@users.noreply.github.com>
2022-09-04 20:43:04 +0300

    Update settings.py

--- userSystem/userSystem/settings.py
+++ userSystem/userSystem/settings.py
@@ -82,8 +82,10 @@ WSGI_APPLICATION = 'userSystem.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-   'defualt': env.db('SQLITE_URL', default='sqlite:///.db.sqlite3'),
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
    'extra': env.db()
     
 }
