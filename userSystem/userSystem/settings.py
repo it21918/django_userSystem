62e1536b3dabf9ec61a33a12d1030131e9d5848a
it21918 <79996442+it21918@users.noreply.github.com>
2022-09-04 20:28:17 +0300

    Update settings.py

--- userSystem/userSystem/settings.py
+++ userSystem/userSystem/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'userSystem.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-   'default': env.db()
+   'default': env.db(),
     
    'extra': env.db('SQLITE_URL', default='sqlite:///.db.sqlite3')
 }
