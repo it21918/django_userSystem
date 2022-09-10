6237175f10c8de44f78c16d3278f263bc66d5a19
Luisdio1 <75575788+Luisdio1@users.noreply.github.com>
2022-09-10 17:27:15 +0300

    Update userViews.py

--- userSystem/base/userViews.py
+++ userSystem/base/userViews.py
@@ -129,6 +129,6 @@ def view_recommendation_letter(request) :
         if (request.status == 'pending'):
             requests.remove(request)
 
-    for x in requests: letters.append(Recommendation_letter.objects.get(request_id = x.id))
+    for x in requests: letters.append(Recommendation_letter.objects.get(request_id != x.id))
 
     return render(request,"student_templates/view_recommendation_letter.html",{"letters":letters} )
