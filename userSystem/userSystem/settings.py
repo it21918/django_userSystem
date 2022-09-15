3a61ecfff0266276097af8ace76ec07b79d7bcfc
it21918 <79996442+it21918@users.noreply.github.com>
2022-09-15 16:48:08 +0300

    Update settings.py

--- userSystem/userSystem/settings.py
+++ userSystem/userSystem/settings.py
@@ -85,9 +85,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite'),
-    },
-   'extra': env.db()
-    
+    }    
 }
 
 # Password validation
