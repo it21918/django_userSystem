e8a15872c0947dfa35b829a00f96fe8517351e4c
it21918 <79996442+it21918@users.noreply.github.com>
2022-09-15 12:42:07 +0300

    Update settings.py

--- userSystem/userSystem/settings.py
+++ userSystem/userSystem/settings.py
@@ -82,9 +82,12 @@ WSGI_APPLICATION = 'userSystem.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-   'default': env.db('SQLITE_URL', default='sqlite:///.db.sqlite3'),
-    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
    'extra': env.db()
+    
 }
 
 # Password validation
